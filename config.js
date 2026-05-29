0ede1cd92ce84db09f1b0a0dddf68fef0a0fd319
kaelpep <samanthapicazo@gmail.com>
2026-05-28 20:04:32 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-// paste your values from supabase project settings > api.
-// the anon public key is expected to be visible in browser apps.
-// do not paste your service role key here.
-
 window.SPLIT_CONFIG = {
   supabaseUrl: "https://drnhtuqhfyouqjwuaame.supabase.co",
-  supabaseAnonKey: "sb_publishable_Cy5ML8RjFnEYSxWGPCqf6Q_jYDl9Wzv"
+  supabaseAnonKey: "sb_publishable_Cy5ML8RjFnEYSxWGPCqf6Q_jYDl9Wzv",
   defaultTripId: "34396aa8-5812-4930-ae3e-4420e51c5230"
 };
