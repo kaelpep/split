e02dd3566bf5abfb6fc079b6ce64f182e095c11a
kaelpep <samanthapicazo@gmail.com>
2026-05-28 19:42:41 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // the anon public key is expected to be visible in browser apps.
 // do not paste your service role key here.
 
-window.TRIP_SPLITTER_CONFIG = {
+window.SPLIT_CONFIG = {
   supabaseUrl: "https://drnhtuqhfyouqjwuaame.supabase.co",
   supabaseAnonKey: "sb_publishable_Cy5ML8RjFnEYSxWGPCqf6Q_jYDl9Wzv"
 };
