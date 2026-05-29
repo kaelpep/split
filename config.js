710bf7fc5bdb7a0e55fdf3eafe6a23d84f352b05
kaelpep <samanthapicazo@gmail.com>
2026-05-28 20:00:39 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 window.SPLIT_CONFIG = {
   supabaseUrl: "https://drnhtuqhfyouqjwuaame.supabase.co",
   supabaseAnonKey: "sb_publishable_Cy5ML8RjFnEYSxWGPCqf6Q_jYDl9Wzv"
-  defaultTripId: "los_angeles"
+  defaultTripId: "34396aa8-5812-4930-ae3e-4420e51c5230"
 };
