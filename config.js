38e9f3e7fa32b0430085e65d6f1464ddc7a65a39
kaelpep <samanthapicazo@gmail.com>
2026-05-28 20:38:38 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.SPLIT_CONFIG = {
+window.TRIP_SPLITTER_CONFIG = {
   supabaseUrl: "https://drnhtuqhfyouqjwuaame.supabase.co",
   supabaseAnonKey: "sb_publishable_Cy5ML8RjFnEYSxWGPCqf6Q_jYDl9Wzv",
   defaultTripId: "34396aa8-5812-4930-ae3e-4420e51c5230"
